e4fe23a80027b7cd088231a9a36f82daea1b66d8
Ethan Johnson <ethanjohnson21888@gmail.com>
2020-08-17 17:04:09 +1200

    Commit files

---
+++ src/main/java/cidemo/Calc.java
@@ -0,0 +1,20 @@
+package cidemo;
+
+public class Calc 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    static int add(int number_one, int number_two)
+    {
+	int output = number_one + number_two;
+	return output;
+    }
+    
+    int subtract(int number_one, int number_two)
+    {
+	return number_one + number_two;
+    }
+}
---
+++ src/test/java/cidemo/CalcTest.java
@@ -0,0 +1,17 @@
+package cidemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalcTest 
+{
+    @Test
+    public void testCalcAdd()
+    {
+        assertTrue( Calc.add(5, 2) == 7 );
+    }
+}
